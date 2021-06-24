dfb0eb718ae962d358801c1ef344f411f5f80e65
Thiago Souza <thlima.silva.souza@gmail.com>
2021-06-24 09:09:40 -0300

    trocando texto h2 do modal de acordo com o botao selecionado

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,10 +2,15 @@ import Modal from './modal.js'
 
 const modal = Modal()
 
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
+
 const checkButtons = document.querySelectorAll(".actions a.check")
 
 checkButtons.forEach(button => {
   button.addEventListener("click", event => {
+    modalTitle.innerHTML = "Marcar como lida"
     modal.open()
   })
 })
@@ -14,6 +19,7 @@ const deleteButton = document.querySelectorAll(".actions a.delete")
 
 deleteButton.forEach(button => {
   button.addEventListener("click", event => {
+    modalTitle.innerHTML = "Excluir essa pergunta"
     modal.open()
   })
 })
