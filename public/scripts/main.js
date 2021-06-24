40c03dffc6bbc196a91c6a9feb9266be7200c233
Thiago Souza <thlima.silva.souza@gmail.com>
2021-06-24 08:45:48 -0300

    corrigindo css para exibir o modal

---
+++ public/scripts/main.js
@@ -0,0 +1,15 @@
+import Modal from './modal.js'
+
+const modal = Modal()
+
+const checkButtons = document.querySelectorAll(".actions a.check")
+
+checkButtons.forEach(button => {
+  button.addEventListener("click", event => {
+    modal.open()
+  })
+})
+
+
+
+
