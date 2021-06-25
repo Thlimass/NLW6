d3d0deb88d451e2a9c65e2979d3a415ee1929a34
Thiago Souza <thlima.silva.souza@gmail.com>
2021-06-24 22:41:30 -0300

    usando o ejs pra unir template

--- src/route.js
+++ src/route.js
@@ -4,9 +4,11 @@ const QuestionController = require('./controllers/QuestionController')
 
 const route = express.Router()
 
-route.get('/', (req, res) => res.render("index"))  
+route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))  
+route.get('/create-pass', (req, res) => res.render("index", {page: 'create-pass'}))
+
 route.get('/room', (req, res) => res.render("room"))
-route.get('/create-pass', (req, res) => res.render("create-pass"))
+
 route.post('/room/:room/:question/:action', QuestionController.index)
 
 module.exports = route
