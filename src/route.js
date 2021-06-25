a911c3f70b7e7e8435a2fb727ed876b3a9b1e41d
Thiago Souza <thlima.silva.souza@gmail.com>
2021-06-24 22:19:09 -0300

    criando rota post

--- src/route.js
+++ src/route.js
@@ -1,10 +1,12 @@
 const { Router } = require('express')
 const express = require('express')
+const QuestionController = require('./controllers/QuestionController')
 
 const route = express.Router()
 
 route.get('/', (req, res) => res.render("index"))  
 route.get('/room', (req, res) => res.render("room"))
 route.get('/create-pass', (req, res) => res.render("create-pass"))
+route.post('/room/:room/:question/:action', QuestionController.index)
 
 module.exports = route
